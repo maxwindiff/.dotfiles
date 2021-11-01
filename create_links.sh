4d10c7ac3c3fc503b264df9fe846d564a037d7b6
Max Ng <maxwindiff@gmail.com>
2021-10-31 17:57:43 -0700

    gvimrc

--- create_links.sh
+++ create_links.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for i in bash_profile bashrc gitconfig vimrc tmux.conf gitignore_global; do
+for i in bash_profile bashrc gitconfig vimrc gvimrc tmux.conf gitignore_global; do
     rm -f ~/.$i
     ln -s $PWD/$i ~/.$i
 done
